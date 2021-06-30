d77ee8dfba02e3cb5d18d4a779677322ccb89ebf
chasers <chase@logflare.app>
2021-06-30 15:18:13 -0700

    Cleanup

--- logger/logger.js
+++ logger/logger.js
@@ -3,6 +3,8 @@ import { logflarePinoVercel } from 'pino-logflare'
 
 // create pino-logflare console stream for serverless functions and send function for browser logs
 // Browser logs are going to: https://logflare.app/sources/13989
+// Vercel log drain was setup to send logs here: https://logflare.app/sources/13830
+
 const { stream, send } = logflarePinoVercel({
     apiKey: "eA_3wro12LpZ",
     sourceToken: "eb1d841a-e0e4-4d23-af61-84465c808157"
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,8 +5,7 @@ import logger from '../../logger/logger'
 export default function handler(req, res) {
   res.status(200)
 
-  // Logging to pino-logflare
-  logger.info({
+  data = {
     requst: {
       method: req.method,
       path: req.path
@@ -14,21 +13,18 @@ export default function handler(req, res) {
     response: {
       status: res.status
     }
-  }, "Handled response. Logged with pino-logflare.")
+  }
+  // Logging to pino-logflare
+  logger.info(data, "Handled response. Logged with pino-logflare.")
 
   // Logging with pino. Both will end up in Vercel's log drains with slight different payloads.
   // Both will end up Logflare if a log drain is setup.
   const onlyPino = require('pino')()
 
-  onlyPino.info({
-    requst: {
-      method: req.method,
-      path: req.path
-    },
-    response: {
-      status: res.status
-    }
-  }, "Handled response. Logged with pino.")
+  onlyPino.info(data, "Handled response. Logged with pino.")
+
+  // We can also simply parse our object into JSON and log it with console.
+  console.info(JSON.parse(data), "Handled response. Logged with `console`.")
 
   res.json({ name: 'John Doe' })
 }
